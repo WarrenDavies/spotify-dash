190e532957c130af36a145ecc6ff28c8ecf35459
W <warren@warrendaviesdev.com>
2019-10-29 19:30:59 +0000

    one click implict grant

--- index.php
+++ index.php
@@ -12,15 +12,10 @@ require('env.php'); ?>
   <script src="jquery-3.4.1.js"></script>
   <link rel="stylesheet" href="style.css?v=1.0">
 </head>
-<body>
+<body onload="checkAuth()">
 <h1>Home</h1>
 
-<br/><br/>
-<br/><br/>
-<br/><br/>
-<a href="https://accounts.spotify.com/authorize?client_id=<?php echo $client_id ?>&redirect_uri=<?php echo $redirect_uri ?>&scope=user-read-private%20user-read-email&response_type=token&state=34fFs29kd09">Get token session (implicit step 1)</a>
-<br/><br/>
-<div id="button3">Get profile (implicit step 2)</div>
+<a href="https://accounts.spotify.com/authorize?client_id=<?php echo $client_id ?>&redirect_uri=<?php echo $redirect_uri ?>&scope=user-read-private%20user-read-email&response_type=token&state=34fFs29kd09">Log in to Spotify</a>
 
 <br/><br/>
 Data: 
