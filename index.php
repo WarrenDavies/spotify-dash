ad576884c06c105b2859d4fa46a7d3117c95ce93
W <warren@warrendaviesdev.com>
2021-03-21 22:17:34 +0000

    now playing

--- index.php
+++ index.php
@@ -1,6 +1,5 @@
 <?php 
 require('env.php'); ?>
-
 <!doctype html>
 
 <html lang="en">
@@ -9,32 +8,63 @@ require('env.php'); ?>
 
   <title>New Site</title>
   <meta name="description" content="Spotify-dash">
-  <script src="jquery-3.4.1.js"></script>
-  <link rel="stylesheet" href="style.css?v=1.0">
+  <script type="text/javascript" src="jquery-3.4.1.js"></script>
+  <!-- <script src="js.js"></script> -->
+  <script type="text/javascript" src="main.js"></script>
+  <link rel="stylesheet" type="text/css" href="style.css">
 </head>
 <body onload="checkAuth()">
 <h1>Home</h1>
 
-<a href="https://accounts.spotify.com/authorize?client_id=<?php echo $client_id ?>&redirect_uri=<?php echo $redirect_uri ?>&scope=user-read-private%20user-read-email%20user-top-read&response_type=token&state=34fFs29kd09">Log in to Spotify</a>
+<a href="https://accounts.spotify.com/authorize?client_id=<?php echo $client_id ?>&redirect_uri=<?php echo $redirect_uri ?>&scope=user-read-private%20user-read-email%20user-top-read%20user-read-currently-playing%20user-read-playback-state&response_type=token&state=34fFs29kd09">Log in to Spotify</a>
 
-<br/><br/>
-User: 
-<div id="user_response">
-
+<div class="container">
+  <div class="holder">
+    <h3>User:</h3>
+    <div id="user_response" class="scroll-y"></div>
+  </div>
+  <div class="holder">
+    <h3>Top Artists</h3>
+    <pre id="artists_response" class="scroll-y"></pre>
+  </div>
+  <div class="holder">
+    <h3>Top Tracks:</h3>
+    <pre id="tracks_response" class="scroll-y"></pre>
+  </div>
 </div>
+<div class="container">
+  <div class="holder">
+    <h3>Top Genres:</h3>
+    <div id="genre_response" class="scroll-y"></div>
+  </div>
+  <div class="holder">
+    <h3>Popularity of your top 50:</h3>
+    <h4>Most Popular:</h4>
+    <div id="top-popularity_response"></div>
+    <h4>Least Popular:</h4>
+    <div id="bottom-popularity_response"></div>
+    <h4>Average Popularity:</h4>
+    <div id="average-popularity_response"></div>
+  </div>
 
-Top Artists: 
-<pre id="artists_response">
-
-</pre>
-
-
-Top Tracks: 
-<pre id="tracks_response">
-
-</pre>
-
-
-<script src="main.js"></script>
+  <div class="holder">
+    <h3>Duration of your top 50 tracks:</h3>
+    <h4>Longest:</h4>
+    <div id="longest-track_response"></div>
+    <h4>Shortest:</h4>
+    <div id="shortest-track_response"></div>
+    <h4>Average:</h4>
+    <div id="average-track_response"></div>
+    <h4>Total:</h4>
+    <div id="total-track_response"></div>
+  </div>
+</div>
+<div class="container">
+  <div class="holder">
+    <h3>Now playing:</h3>
+    <div id="now-playing"></div>
+  </div>
+</div>
+<!-- <script src="main.js"></script> -->
 </body>
 </html>
--- main.js
+++ main.js
@@ -1,3 +1,9 @@
+function millisToMinutesAndSeconds(millis) {
+    var minutes = Math.floor(millis / 60000);
+    var seconds = ((millis % 60000) / 1000).toFixed(0);
+    return minutes + ":" + (seconds < 10 ? '0' : '') + seconds;
+}
+
 function checkAuth() {
     if (window.location.hash.substr(1) != "") {
         let type = window.location.hash.substr(1);
@@ -9,39 +15,150 @@ function checkAuth() {
             }
         });
         $.ajax({
+            url: 'https://api.spotify.com/v1/me/player',
+            headers:{'Authorization':'Bearer ' + token},
+            complete: function(data) {
+                console.log("player");
+                console.log(data);
+                $("#now-playing").html(JSON.stringify(data));
+            }
+        });
+        $.ajax({
             url: 'https://api.spotify.com/v1/me/top/artists?limit=50&time_range=long_term',
             headers:{'Authorization':'Bearer ' + token},
             complete: function(data) {
-                console.log(data);
+                
+                // console.log(data);
                 dataString = JSON.stringify(data);
                 dataBreaks = dataString.replace(/\\n/g, "<br/>");
                 dataBreaks = dataBreaks.replace(/\\/g, " ");
                 let artists = "";
+                let artistsData = [];
+                let artistGenres = [];
+                let artistPopularity = [];
+                let averagePopularity = 0;
+                let artistGenreCount = [];
+                console.log("artists");
                 console.log(data.responseJSON.items);
                 data.responseJSON.items.forEach(function(i, j) {
                     if (i.name) {
                         artists += i.name + "<br/>"
+                        artistsData.push({
+                            name: i.name,
+                            popularity: i.popularity,
+                        })
+                    }
+                    if (i.genres) {
+                        i.genres.forEach(function(k, l){
+                            if (artistGenres.length == 0) {
+                                artistGenres.push({
+                                    name: k,
+                                    count: 1
+                                });
+                            } else {
+                                let found = false;
+                                for(var m = 0; m < artistGenres.length; m++) {
+                                    if (artistGenres[m].name == k) {
+                                        artistGenres[m].count += 1
+                                        found = true;
+                                        break;
+                                    }
+                                }
+                                if (found == false) {
+                                    artistGenres.push({
+                                        name: k,
+                                        count: 1
+                                    });
+                                }
+                            }
+                        });
+                    }
+                    if (i.popularity) {
+                        artistPopularity.push(i.popularity);
                     }
                 });
+                artistGenres.sort((a, b) => {
+                    let order1 = b.count - a.count;
+                    if (order1 !== 0) {
+                        return order1;
+                    } 
+                    return a.name - b.name
+                });
+                artistsData.sort((a, b) => {
+                    let order1 = b.popularity - a.popularity;
+                    if (order1 !== 0) {
+                        return order1;
+                    } 
+                    return a.name - b.name
+                });
+                console.log("artistGenres:");
+                console.log(artistGenres);
+                let genreList = "";
+                artistGenres.forEach(function(i, j) {
+                    genreList += i.name + " (" + i.count + ") <br/>";
+                })
+                artistPopularity.forEach(function(i, j) {
+                    averagePopularity += i;
+                })
+                averagePopularity = averagePopularity / artistPopularity.length;
+                
+                let topPopularity = artistsData[0].popularity + " (" + artistsData[0].name + ")";
+                
+                let bottomPopularity = artistsData[artistsData.length - 1].popularity + " (" + artistsData[artistsData.length - 1].name + ")";
+
+
                 $("#artists_response").html(artists);
+                $("#genre_response").html(genreList);
+
+                $("#top-popularity_response").html(topPopularity);
+                $("#bottom-popularity_response").html(bottomPopularity);
+                $("#average-popularity_response").html(averagePopularity);
+                
             }
         });
         $.ajax({
             url: 'https://api.spotify.com/v1/me/top/tracks?limit=50&time_range=long_term',
             headers:{'Authorization':'Bearer ' + token},
             complete: function(data) {
+                console.log("tracks:");
                 console.log(data);
                 dataString = JSON.stringify(data);
                 dataBreaks = dataString.replace(/\\n/g, "<br/>");
                 dataBreaks = dataBreaks.replace(/\\/g, " ");
                 let tracks = "";
+                let tracksData = [];
+                let averageDuration = 0;
+                let totalDuration = 0;
                 console.log(data.responseJSON.items);
                 data.responseJSON.items.forEach(function(i, j) {
                     if (i.name) {
                         tracks += i.name + "<br/>"
                     }
+                    tracksData.push({
+                        name: i.name,
+                        popularity: i.popularity,
+                        duration: i.duration_ms
+                    })
+                    totalDuration += i.duration_ms;
                 });
                 $("#tracks_response").html(tracks);
+                
+                tracksData.sort((a, b) => {
+                    let order1 = b.duration - a.duration;
+                    if (order1 !== 0) {
+                        return order1;
+                    } 
+                    return a.name - b.name
+                });
+                totalDurationTime = millisToMinutesAndSeconds(totalDuration);
+
+                averageDuration = millisToMinutesAndSeconds(totalDuration / tracksData.length);
+                let topTrackDuration = millisToMinutesAndSeconds(tracksData[0].duration) + " (" + tracksData[0].name + ")";
+                let bottomTrackDuration = millisToMinutesAndSeconds(tracksData[tracksData.length - 1].duration) + " (" + tracksData[tracksData.length - 1].name + ")";
+                $("#longest-track_response").html(topTrackDuration);
+                $("#shortest-track_response").html(bottomTrackDuration);
+                $("#average-track_response").html(averageDuration);
+                $("#total-track_response").html(totalDurationTime);
             }
         });
     } else {
